c977c65b5e09cada3915be48f6702e22ceda1cd1
unknown <himanshupatel06@gmail.com>
2016-02-02 18:57:26 -0500

    updated server.js to run express

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 /// < reference path= "_reference.ts" />
-var http = require('http');
+var express = require('express');
 var port = process.env.port || 3000;
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-type': 'text/plain' });
-    res.end("Hello node");
+var app = express();
+app.get('/', function (req, res, next) {
+    res.send('Hello Express!');
 });
-server.listen(port, function () {
-    console.log("Server started..listening on port.." + port);
+app.get('/info', function (req, res) {
+    res.send('Custom info page..');
 });
+app.listen(port, function () {
+    console.log("App Server started..On port.." + port);
+});
+/*
+import http = require('http');
+
+var port:number = process.env.port || 3000 ;
+var server = http.createServer(function(req:http.ServerRequest, res:http.ServerResponse){
+     
+     res.writeHead(200, {'Content-type': 'text/plain'});
+     
+     res.end("Hello node");
+});
+
+
+server.listen(port, function()
+{
+   console.log("Server started..listening on port.." +port);
+});
+*/ 
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,5 +1,22 @@
 /// < reference path= "_reference.ts" />
 
+import express = require('express');
+var port:number = process.env.port || 3000 ; 
+var app:express.Express = express();
+
+app.get('/', function (req:express.Request, res:express.Response, next:any) {
+  res.send('Hello Express!');
+});
+
+app.get('/info', function (req:express.Request, res:express.Response) {
+  res.send('Custom info page..');
+});
+
+app.listen(port, function()
+{
+   console.log("App Server started..On port.." +port);
+});
+/*
 import http = require('http');
 
 var port:number = process.env.port || 3000 ; 
@@ -15,3 +32,4 @@ server.listen(port, function()
 {
    console.log("Server started..listening on port.." +port);
 });
+*/
